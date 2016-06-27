ad4c9a92173dc1c37197e17fd333bcaa085f1103
Gaetan Semet <gaetan@xeberon.net>
2016-06-27 18:22:44 +0200

    Keep only python project
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(
-    name='sparktestingbase',
-    version='0.0.7-snapshot',
+    name='spark-testing-base',
+    version='0.0.7-2',
     author='Holden Karau',
     author_email='holden@pigscanfly.ca',
     packages=['sparktestingbase', 'sparktestingbase.test'],
-    url='https://github.com/holdenk/spark-testing-base',
+    url='https://github.com/Stibbons/python-spark-testing-base',
+    # Fork of 'https://github.com/holdenk/spark-testing-base',
     license='LICENSE.txt',
+    long_description=readme(),
     description='Spark testing for python',
-    long_description='',
     install_requires=[
         'unittest2',
         'findspark',
--- src/test/1.3/scala/com/holdenkarau/spark/testing/MultipleDataFrameSuites.scala
+++
@@ -1,9 +0,0 @@
-package com.holdenkarau.spark.testing
-
-import org.scalatest.FunSuite
-
-class MultipleDataFrameSuites extends FunSuite with DataFrameSuiteBase {
-  test("test nothing") {
-    assert(1 === 1)
-  }
-}
